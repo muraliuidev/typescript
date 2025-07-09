e8c8328d5b210894031cdc868749e423ae044ea6
muraliuidev <muralikonapala.uidev@gmail.com>
2025-07-09 10:16:50 +0530

    js file

--- index.js
+++ index.js
@@ -6,8 +6,33 @@ const menu = [
 ]
 
 const cashInRegister = 100
+const nextOrderId = 1
 const orderQueue = []
 
 function addNewItem(itemObj){
     menu.push(itemObj)
 }   
+
+function placeOrder(itemName) {
+    const selectedItem = menu.find(itemObj => itemObj.name === itemName)
+    cashInRegister += selectedItem.price
+    const newOrder = { id: nextOrderId++, Item: selectedItem, status: 'ordered' }
+    orderQueue.push(newOrder)
+    return newOrder
+}
+
+function completeOrder (orderId) {
+    const order = orderQueue.find(order => order.id === orderId)
+    order.status = "completed"
+    return order
+}
+
+addNewItem({ name: "Upma", price: 10 })
+addNewItem({ name: "Utappam", price: 15 })
+
+placeOrder("Utappam")
+completeOrder("1")
+
+console.log("Menu:", menu)
+console.log("Cash in register:", cashInRegister)
+console.log("Order queue:", orderQueue)
